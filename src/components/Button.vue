f3d3cf93247fb6648b585eddb88f1697c3ad3ebf
Oscar Moreno <oscar@pidedirecto.mx>
2024-10-19 18:42:05 -0600

    add Button

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{ class?: string }>()
+const emit = defineEmits(['click'])
+
+const handleClick = e => {
+    emit('click')
+}
+
+</script>
+<template>
+    <button class="button" :class="props.class" @click="handleClick">
+        <slot />
+    </button>
+</template>
+<style scoped>
+.button {
+    border: 1px solid white;
+    border-radius: 6px;
+    padding: 4px 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 4px;
+    justify-content: center;
+    font-size: 20px;
+    font-family: 'Oswald', sans-serif;
+    cursor: pointer;
+}
+</style>
