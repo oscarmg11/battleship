85e32bd35b467c1dee583e1d1156e8fa32c1f8a9
Oscar Moreno <oscar@pidedirecto.mx>
2024-10-19 18:42:14 -0600

    add Menu

---
+++ src/components/app/Menu.vue
@@ -0,0 +1,26 @@
+<script setup lang='ts'>
+import Button from '@/components/Button.vue'
+import { camera } from '@/game/Camera.ts'
+
+const setupShips = () => {
+    camera.moveTo({ x: 0, y: 10, z: 0 })
+}
+
+</script>
+
+<template>
+    <nav class="nav">
+        <Button @click='setupShips'>Setup ships</Button>
+    </nav>
+</template>
+
+<style scoped>
+.nav {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    position: absolute;
+    right: 10vw;
+    top: 10vh;
+}
+</style>
