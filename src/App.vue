2a595d289d271efd3b05c0a4ca18bf897302bfb5
Oscar Moreno <oscar@pidedirecto.mx>
2024-10-20 20:33:05 -0600

    remove unused code in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import Menu from '@/components/app/Menu.vue'
+
+onMounted(() => {
+
+})
 
 </script>
 
 <template>
-
+    <Menu />
 </template>
