9fbf30916d4002a48da00d997a7ecc175fb9ba43
Oscar Moreno <oscar@pidedirecto.mx>
2024-10-24 22:20:17 -0600

    add text component

---
+++ src/components/Text.vue
@@ -0,0 +1,25 @@
+<script setup lang='ts'>
+const props = defineProps<{ variant?: 'title', class?: string }>()
+
+</script>
+
+<template>
+    <span class='text' :class="[{ title: props.variant === 'title' }, props.class]">
+        <slot />
+    </span>
+</template>
+
+<style scoped>
+    .text {
+        font-size: 20px;
+        font-family: 'Oswald', sans-serif;
+        color: white;
+    }
+
+    .title {
+        font-size: 24px;
+        font-family: 'Oswald', sans-serif;
+        color: white;
+        font-weight: 600;
+    }
+</style>
