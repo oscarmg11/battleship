16d2032df0fc576ba47b5050bf43063f7c88a3bf
Oscar Moreno <oscar@pidedirecto.mx>
2024-10-24 20:24:14 -0600

    add onDragShip BoardEventListener

--- src/utils/event/BoardEventListener.ts
+++ src/utils/event/BoardEventListener.ts
@@ -5,14 +5,16 @@ import { Point } from '@/types/Point.ts'
 export class BoardEventListener extends EventListener {
 
     static addEventListener(event: 'onDropShip', fn: (data:number) => void): () => void
+    static addEventListener(event: 'onDragShip', fn: (data:number) => void): () => void
     static addEventListener(event: Event, fn: (data: any) => void) {
         return super.addEventListener(event, fn)
     }
 
     static updateEventListener(event: 'onDropShip', data:number): void
+    static updateEventListener(event: 'onDragShip', data:number): void
     static updateEventListener(event: Event, data: any) {
         super.updateEventListener(event, data)
     }
 }
 
-type Event = 'onDropShip'
+type Event = 'onDropShip' | 'onDragShip'
