093208d1405c2912685f9f25e6d2bd364e3ab0df
Oscar Moreno <oscar@pidedirecto.mx>
2024-10-24 20:51:06 -0600

    add i18n support

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
+import { i18n } from '@/locales/i18n.ts'
 
 const app = createApp(App)
 
 app.use(createPinia())
-
+app.use(i18n)
 app.mount('#app')
