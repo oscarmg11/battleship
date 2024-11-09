6f277fbd2361bcdbd18cbc721be0a8b7fb873451
Oscar Moreno <oscar@pidedirecto.mx>
2024-11-09 16:30:03 -0600

    add Loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,27 @@
+<script setup lang='ts'>
+    const props = defineProps<{ size?: number }>()
+</script>
+
+<template>
+    <span class="loader" :style="{ height: (props.size ?? 30) + 'px', width: (props.size ?? 30) + 'px' }"></span>
+</template>
+
+<style scoped>
+.loader {
+    border: 2px solid #FFF;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
