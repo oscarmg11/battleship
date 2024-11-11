b17e11ee338466114d57197b53ea45c47079fcfe
Oscar Moreno <oscar@pidedirecto.mx>
2024-11-10 22:43:02 -0600

    improve WebSockets

--- src/utils/event/WebSocketEventListener.ts
+++ src/utils/event/WebSocketEventListener.ts
@@ -1,18 +1,21 @@
 import { EventListener } from '@/utils/event/EventListener.ts'
 import * as THREE from 'three'
 import { Point } from '@/game/types/Point.ts'
+import { GameVm } from '@/types/GameVm.js'
 
 export class WebSocketEventListener extends EventListener {
 
+    static addEventListener(event: 'onGameCreated', fn: (game: GameVm) => void): () => void
     static addEventListener(event: 'onEnemyReady', fn: () => void): () => void
-    static addEventListener(event: Event, fn: (data: any) => void) {
+    static addEventListener(event: WebSocketEvent, fn: (data: any) => void) {
         return super.addEventListener(event, fn)
     }
 
+    static updateEventListener(event: 'onGameCreated', game: GameVm): void
     static updateEventListener(event: 'onEnemyReady'): void
-    static updateEventListener(event: Event, data?: any) {
+    static updateEventListener(event: WebSocketEvent, data?: any) {
         super.updateEventListener(event, data)
     }
 }
 
-type Event = 'onEnemyReady'
+export type WebSocketEvent = 'onEnemyReady' | 'onGameCreated'
---
+++ src/webSocket/WebSocket.ts
@@ -0,0 +1,42 @@
+import { WebSocketEvent, WebSocketEventListener } from '@/utils/event/WebSocketEventListener.js'
+
+class WebSocketImplementation {
+    webSocketConnection: WebSocket
+    constructor() {
+        const webSocketConnection = new WebSocket(import.meta.env.VITE_WEBSOCKET_URI)
+        webSocketConnection.onopen = (event) => {
+            console.log('WebSocket connected');
+        };
+
+        webSocketConnection.onmessage = (event: MessageEvent<string>) => {
+            const eventMessage = JSON.parse(event.data) as WebSocketEventMessage;
+            WebSocketEventListener.updateEventListener(eventMessage.event as any, eventMessage.data)
+        };
+
+        webSocketConnection.onclose = () => {
+            console.log('WebSocket connection closed');
+        };
+
+        webSocketConnection.onerror = (error) => {
+            console.error('WebSocket error:', error);
+        };
+        this.webSocketConnection = webSocketConnection
+    }
+
+    sendEvent(event: EventSent, data?: any){
+        this.webSocketConnection.send(JSON.stringify({
+            event,
+            data
+        }))
+    }
+
+}
+
+export const webSocket = new WebSocketImplementation()
+
+type EventSent = 'connectHostToGame'
+
+type WebSocketEventMessage = {
+    event: WebSocketEvent;
+    data: any
+}
