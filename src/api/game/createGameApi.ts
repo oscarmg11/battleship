4803f783a075cff27de88a7593b36dfdd57ac0e8
Oscar Moreno <oscar@pidedirecto.mx>
2025-01-13 20:51:36 -0600

    add playerId to PlayerVm + local storage function

--- src/api/game/createGameApi.ts
+++ src/api/game/createGameApi.ts
@@ -2,12 +2,16 @@ import { GameVm } from '@/types/GameVm.js'
 import axios from 'axios'
 import { envViteGameApi } from '@/utils/env/envViteGameApi.js'
 
-export async function createGameApi(): Promise<GameVm>{
+export async function createGameApi(request: Request): Promise<GameVm>{
     const response = await axios<GameVm>({
         method: 'POST',
-        url: `${envViteGameApi()}/game/createGame`
+        url: `${envViteGameApi()}/game/createGame`,
+        data: request
     })
 
     return response.data
+}
 
+type Request = {
+    playerId: string
 }
