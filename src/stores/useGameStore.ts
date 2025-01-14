4d3bbf1ca29896fed059b758bfd65a1b25d15ee0
Oscar Moreno <oscar@pidedirecto.mx>
2025-01-13 20:30:17 -0600

    add enable and disable methods in useGameStore

--- src/stores/useGameStore.ts
+++ src/stores/useGameStore.ts
@@ -3,19 +3,50 @@ import { defineStore } from 'pinia'
 import { GameVm } from '@/types/GameVm.js'
 import { getGameFromSessionStorage } from '@/utils/sessionStorage/game/getGameFromSessionStorage.ts'
 import { setGameInSessionStorage } from '@/utils/sessionStorage/game/setGameInSessionStorage.ts'
+import { game as gameObject } from '@/game/Game.ts'
 
 export const useGameStore: Store = defineStore('game', () => {
     const game = ref<undefined | GameVm>(getGameFromSessionStorage())
+    const disabled = ref(true)
+    const settingUp = ref(false)
 
     function setGame(newGame: GameVm) {
         setGameInSessionStorage(newGame)
         game.value = newGame
     }
 
-    return { game, setGame }
+    function enable(){
+        disabled.value = false
+        gameObject.enable()
+    }
+
+    function disable(){
+        disabled.value = true
+        gameObject.disable()
+    }
+
+    function setupGame(){
+        if(settingUp.value) return
+
+        settingUp.value = true
+        gameObject.setupGame()
+    }
+
+    function finishSetupGame(){
+        settingUp.value = false
+        gameObject.finishSetupGame()
+    }
+
+    return { game, disabled, settingUp, setGame, enable, disable, setupGame, finishSetupGame }
 })
 
 type Store = () => ({
     game: GameVm | undefined,
+    disabled: boolean,
+    settingUp: boolean,
     setGame: (game: GameVm) => void,
+    enable: () => void,
+    disable: () => void,
+    setupGame: () => void,
+    finishSetupGame: () => void,
 })
