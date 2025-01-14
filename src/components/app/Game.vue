00c1e7454982a97c74fae9cdcce03064759eeb13
Oscar Moreno <oscar@pidedirecto.mx>
2025-01-13 21:18:53 -0600

    add player ids to game + improve game enabling

--- src/components/app/Game.vue
+++ src/components/app/Game.vue
@@ -1,23 +1,35 @@
-<script setup>
+<script setup lang='ts'>
 import GameMenu from '@/components/app/GameMenu.vue'
 import GameHome from '@/components/app/GameHome.vue'
 import { onMounted, ref } from 'vue'
 import { WebSocketEventListener } from '@/utils/event/WebSocketEventListener'
 import { useGameMessagesStore } from '@/stores/useGameMessagesStore'
 import { translate } from '@/utils/text/translate'
+import { useGameStore } from '@/stores/useGameStore'
+import { SessionVm } from '@/types/SessionVm.js'
+import { usePlayerStore } from '@/stores/usePlayerStore'
+import { createPlayerIdInLocalStorage } from '@/utils/localStorage/createPlayerIdInLocalStorage'
 
 const gameInitialized = ref(false)
 
+const gameStore = useGameStore()
+const playerStore = usePlayerStore()
 const gameMessagesStore = useGameMessagesStore()
 
 onMounted(() => {
-    WebSocketEventListener.addEventListener('onRivalConnected', () => {
-        gameMessagesStore.showGameMessage({
+    const playerId = createPlayerIdInLocalStorage()
+    playerStore.setPlayerId(playerId)
+
+    WebSocketEventListener.addEventListener('onRivalConnected', async () => {
+        gameStore.enable()
+        await gameMessagesStore.showGameMessage({
             message: translate('Rival connected')
         })
+        gameStore.setupGame()
     })
 
     WebSocketEventListener.addEventListener('onRivalDisconnected', () => {
+        gameStore.disable()
         gameMessagesStore.showGameMessage({
             message: translate('Rival disconnected')
         })
@@ -27,15 +39,32 @@ onMounted(() => {
             lastUntilNewMessage: true
         })
     })
+
+    WebSocketEventListener.addEventListener('onSessionInfo', (session: SessionVm) => {
+        playerStore.setSession(session)
+    })
 })
 
-const handleGameCreated = () => {
+const handleGameCreated = (data?: { isGameSettingUp: boolean }) => {
     gameInitialized.value = true
+    if(data?.isGameSettingUp) {
+        gameStore.enable()
+        gameStore.setupGame()
+    }
+}
+
+const handleJoinedToGame = async () => {
+    gameStore.enable()
+    gameInitialized.value = true
+    await gameMessagesStore.showGameMessage({
+        message: translate('Connected')
+    })
+    gameStore.setupGame()
 }
 
 </script>
 
 <template>
     <GameMenu v-if='gameInitialized' />
-    <GameHome v-if='!gameInitialized' @onGameCreated='handleGameCreated' />
+    <GameHome v-if='!gameInitialized' @on-game-created='handleGameCreated' @on-joined-to-game='handleJoinedToGame' />
 </template>
